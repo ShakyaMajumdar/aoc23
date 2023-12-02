8e018d40ed9c625d84971f33e019c96c4a0c31e9
Shakya Majumdar <shakyamajumdar1@gmail.com>
2023-12-02 18:14:18 +0530

    refactor

--- src/day2/Day2.hs
+++ src/day2/Day2.hs
@@ -1,23 +1,9 @@
 import Data.Char (isDigit)
-import Data.List ( find, isInfixOf )
+import Data.List (find, isInfixOf)
 import Data.List.Split (splitOn)
 import Data.Maybe (listToMaybe)
 
 type RGB = (Int, Int, Int)
-
-type Game = (Int, [RGB])
-
-main :: IO ()
-main = do
-  contents <- readFile "input.txt"
-  print $ part1 contents
-  print $ part2 contents
-
-part1 :: String -> Int
-part1 s = sum $ map fst $ filter isValid $ map parseGame (lines s)
-
-part2 :: String -> Int
-part2 s = sum $ map (getPower . parseGame) (lines s)
 
 red :: RGB -> Int
 red (r, g, b) = r
@@ -28,22 +14,34 @@ green (r, g, b) = g
 blue :: RGB -> Int
 blue (r, g, b) = b
 
+type Game = (Int, [RGB])
+
+main :: IO ()
+main = do
+  contents <- readFile "input.txt"
+  print $ part1 contents
+  print $ part2 contents
+
+-- PART 1 --
+part1 :: String -> Int
+part1 s = sum $ map fst $ filter isValid $ map parseGame (lines s)
+
 parseGame :: String -> Game
 -- s = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
-parseGame s = do
+parseGame s =
   let p = splitOn ": " s
-  let id' = read $ drop (length "Game ") (head p) :: Int
-  let subs = splitOn "; " (last p)
-  (id', map parseSingle subs)
+      id' = read $ drop (length "Game ") (head p) :: Int
+      subs = splitOn "; " (last p)
+   in (id', map parseSingle subs)
 
 parseSingle :: String -> (Int, Int, Int)
 -- s = "3 blue, 4 red"
-parseSingle s = do
+parseSingle s =
   let parts = splitOn ", " s
-  let red = extractColour "red" parts
-  let green = extractColour "green" parts
-  let blue = extractColour "blue" parts
-  (red, green, blue)
+      red = extractColour "red" parts
+      green = extractColour "green" parts
+      blue = extractColour "blue" parts
+   in (red, green, blue)
 
 extractColour :: String -> [String] -> Int
 extractColour colour parts = extractNum $ find (colour `isInfixOf`) parts
@@ -53,15 +51,15 @@ extractNum Nothing = 0
 extractNum (Just s) = read $ takeWhile isDigit s
 
 isValid :: Game -> Bool
-isValid (_, subs) = do
-  let rmax = maximum $ map red subs
-  let gmax = maximum $ map green subs
-  let bmax = maximum $ map blue subs
-  rmax <= 12 && gmax <= 13 && bmax <= 14
+isValid (_, subs) =
+  let most c = maximum $ map c subs
+   in (most red <= 12) && (most green <= 13) && (most blue <= 14)
+
+-- PART 2 --
+part2 :: String -> Int
+part2 s = sum $ map (getPower . parseGame) (lines s)
 
 getPower :: Game -> Int
-getPower (_, subs) = do
-  let rmax = maximum $ map red subs
-  let gmax = maximum $ map green subs
-  let bmax = maximum $ map blue subs
-  rmax * gmax * bmax
+getPower (_, subs) =
+  let most c = maximum $ map c subs
+   in most red * most blue * most green
